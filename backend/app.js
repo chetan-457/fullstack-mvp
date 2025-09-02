30ca49f8cd3431e13b6f62c1bf62ed0b2acc05e4
Chetan Marwadi <chetan19@groovyweb.firm.in>
2025-09-02 10:28:12 +0530

    🆕: Fullstack hello world with Postgres

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dbtestRouter = require('./routes/dbtest');
+// var dbtestRouter = require('./routes/dbtest');
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', dbtestRouter);
+app.use('/api', usersRouter);
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+// backend/routes/users.js
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'user',
+  host: 'localhost',
+  database: 'mydb',
+  password: 'pass',
+  port: 5432,
+});
+
+router.get('/users', async (req, res) => {
+  const { rows } = await pool.query('SELECT * FROM users');
+  res.json(rows);
 });
 
 module.exports = router;
