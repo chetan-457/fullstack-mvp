8606fcf215e3f0b41e80c81bc184d4d7a5fe3b08
Chetan Marwadi <chetan19@groovyweb.firm.in>
2025-09-02 12:54:19 +0530

    🔨: Cors updated for backed with prod

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 const allowedOrigins = [
     'http://localhost:3001',
     'https://fullstack-mvp-r1vo-kwk4bxz1n-marwadichetan140-2519s-projects.vercel.app',
-    'https://fullstack-mvp-745r.onrender.com'
+    'https://fullstack-mvp-745r.onrender.com',
+    'https://fullstack-mvp-r1vo-51og5t0v7-marwadichetan140-2519s-projects.vercel.app/'
 ];
 
 var indexRouter = require('./routes/index');
